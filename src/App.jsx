b7cf25b7bc01386319f46a0836e468d28bd46e80
andresilva05 <andresilva9657@gmail.com>
2025-01-04 17:00:17 -0300

    Criar componente App com importação de estilos globais e seção de introdução

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Container } from "./styles";
+import GlobalStyles from "./globalStyles";
+import IntroSection from "./IntroSection";
+
+export default function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <Container />
+      <IntroSection />
+    </>
+  );
+}
