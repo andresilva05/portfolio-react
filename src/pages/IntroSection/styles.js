9f69f6af176d4e6a623376ae9788abeb5d58c705
andresilva05 <andresilva9657@gmail.com>
2025-01-14 20:37:56 -0300

    feat: melhorar responsividade e adicionar animação de fade-in no nome

--- src/pages/IntroSection/styles.js
+++ src/pages/IntroSection/styles.js
@@ -1,12 +1,26 @@
 import styled from "styled-components";
 
-export const ContainerHeader = styled.header``;
+export const ContainerHeader = styled.header`
+  position: relative;
+  z-index: 1;
+  width: 100%;
+  height: 100%;
+`;
+
 export const Title = styled.h2`
   color: #ffffff;
   font-size: 35px;
   font-weight: 500;
   span {
     color: #fa0707;
+  }
+
+  @media (max-width: 768px) {
+    font-size: 30px;
+  }
+
+  @media (max-width: 480px) {
+    font-size: 25px;
   }
 `;
 
@@ -17,12 +31,32 @@ export const StyledNav = styled.nav`
   padding-left: 30px;
   padding-right: 30px;
   padding-top: 25px;
+
+  @media (max-width: 768px) {
+    padding-left: 20px;
+    padding-right: 20px;
+    padding-top: 20px;
+  }
+
+  @media (max-width: 480px) {
+    padding-left: 15px;
+    padding-right: 15px;
+    padding-top: 15px;
+  }
 `;
 
 export const NavList = styled.ul`
   list-style: none;
   display: flex;
   gap: 50px;
+
+  @media (max-width: 768px) {
+    gap: 30px;
+  }
+
+  @media (max-width: 480px) {
+    gap: 20px;
+  }
 `;
 
 export const NavItem = styled.li`
@@ -30,30 +64,85 @@ export const NavItem = styled.li`
     text-decoration: none;
     color: #fff;
     font-size: 25px;
+    transition: transform 0.2s ease-in-out, color 0.5s; /* Suaviza transform e a mudança de cor */
   }
+
   a:hover {
-    color: #fa0707;
-    transition: 0.5s;
+    color: #fa0707; /* Altera a cor ao passar o mouse */
+    transform: scale(1); /* Aplica o efeito de pulse */
   }
+
   .home {
     display: none;
+  }
+
+  @media (max-width: 768px) {
+    a {
+      font-size: 20px;
+    }
+  }
+
+  @media (max-width: 480px) {
+    a {
+      font-size: 18px;
+    }
   }
 `;
 
 export const MyName = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh; /* Preenche toda a altura da tela */
+  text-align: center; /* Centraliza o texto */
   pointer-events: none;
-  margin: 340px 0 40px 20px;
 
   h1 {
     color: white;
     font-size: 60px;
+    opacity: 0;
+    transform: translateY(20px);
+    animation: fadeIn 2s ease-in-out forwards; /* Animação */
 
     span {
       color: #fa0707;
     }
   }
+
   h3 {
     color: white;
     font-size: 25px;
+  }
+
+  @media (max-width: 768px) {
+    h1 {
+      font-size: 50px;
+    }
+
+    h3 {
+      font-size: 20px;
+    }
+  }
+
+  @media (max-width: 480px) {
+    h1 {
+      font-size: 40px;
+    }
+
+    h3 {
+      font-size: 18px;
+    }
+  }
+
+  @keyframes fadeIn {
+    0% {
+      opacity: 0;
+      transform: translateY(20px);
+    }
+    100% {
+      opacity: 1;
+      transform: translateY(0);
+    }
   }
 `;
