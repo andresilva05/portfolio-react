dcb94172f6852f6473856dc0a9c5139b461ead81
andresilva05 <andresilva9657@gmail.com>
2025-01-14 20:40:44 -0300

    feat: adicionar seções de projetos principais e rodapé ao layout
    - Incluídas as seções "MainProjects" e "Footer" ao componente principal "App".
    - A estrutura do layout agora inclui a introdução, informações sobre mim, projetos principais e o rodapé.
    - A navegação foi organizada para exibir as seções de forma sequencial.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import GlobalStyles from "./globalStyles";
 import IntroSection from "./pages/IntroSection";
 import AboutMe from "./pages/AboutMe";
 
+import MainProjects from "./pages/MainProjects";
+import Footer from "./pages/Footer";
+
 export default function App() {
   return (
     <>
       <GlobalStyles />
       <IntroSection />
       <AboutMe />
+      <MainProjects />
+      <Footer/>
     </>
   );
 }
