45306fb87f901d141c2890b98ba1bbaee9817df0
andresilva05 <andresilva9657@gmail.com>
2025-01-14 20:37:30 -0300

    feat: adicionar animação de cor no nome e animação de digitação para profissão

--- src/pages/IntroSection/index.jsx
+++ src/pages/IntroSection/index.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from "react";
 import {
   ContainerHeader,
   MyName,
@@ -6,38 +7,74 @@ import {
   StyledNav,
   Title,
 } from "./styles";
+import TypingAnimation from "../../components/TypingContainer";
+// import Particles from "react-tsparticles
+
+// import ParticlesBackground from "../../components/ParticlesBackground";
 
 export default function IntroSection() {
-  return (
-      <ContainerHeader>
-        <StyledNav>
-          <Title>
-            Portfo<span>lio</span>
-          </Title>
+  const headingRef = useRef(null); // Referência para o <h1>
+  const [silvaColor, setSilvaColor] = useState("#fa0707"); // Estado para a cor de "Silva"
+  // const [desenvolvedorColor, setDesenvolvedorColor] = useState("#ffffff"); // Estado para a cor de "Programador"
 
-          <NavList>
-            <NavItem>
-              <a className="home" href="#inicio">
-                Home
-              </a>
-            </NavItem>
-            <NavItem>
-              <a href="#sobre">Sobre</a>
-            </NavItem>
-            <NavItem>
-              <a href="#projetos">Projetos</a>
-            </NavItem>
-            <NavItem>
-              <a href="#contatos">Fale comigo</a>
-            </NavItem>
-          </NavList>
-        </StyledNav>
-        <MyName>
-          <h1>
-            André <span>Silva</span>
-          </h1>
-          <h3>Programador Front End</h3>
-        </MyName>
-      </ContainerHeader>
+  useEffect(() => {
+    const h1Element = headingRef.current;
+
+    if (h1Element) {
+      // Adiciona a classe `visible` após um pequeno delay
+      setTimeout(() => {
+        h1Element.classList.add("visible");
+      }, 100); // Ajuste o delay se necessário
+    }
+
+    // Alterna as cores de "Silva"
+    const silvaInterval = setInterval(() => {
+      setSilvaColor((prevColor) =>
+        prevColor === "#fa0707" ? "#0e274b" : "#fa0707"
+      );
+    }, 2000);
+
+
+
+    // Limpa os intervalos ao desmontar o componente
+    return () => {
+      clearInterval(silvaInterval);
+    };
+  }, []);
+
+  return (
+    <ContainerHeader>
+      <StyledNav>
+        <Title>
+          Portfo<span>lio</span>
+        </Title>
+
+        <NavList>
+          <NavItem>
+            <a className="home" href="#inicio">
+              Home
+            </a>
+          </NavItem>
+          <NavItem>
+            <a href="#sobre">Sobre</a>
+          </NavItem>
+          <NavItem>
+            <a href="#projetos">Projetos</a>
+          </NavItem>
+          <NavItem>
+            <a href="#contato">Fale comigo</a>
+          </NavItem>
+        </NavList>
+      </StyledNav>
+      <MyName>
+        <h1>
+          Olá, sou <br />
+        </h1>
+        <h1 ref={headingRef}>
+          André <span style={{ color: silvaColor }}>Silva</span>
+        </h1>
+        <TypingAnimation />
+      </MyName>
+    </ContainerHeader>
   );
 }
