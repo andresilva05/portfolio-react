74dd7c936c4ef24ca99f15de35cc41cd411a344c
andresilva05 <andresilva9657@gmail.com>
2025-01-16 22:38:08 -0300

    style: ajusta espaçamento e formatação no componente App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import GlobalStyles from "./globalStyles";
 import IntroSection from "./pages/IntroSection";
 import AboutMe from "./pages/AboutMe";
-
 import MainProjects from "./pages/MainProjects";
 import Footer from "./pages/Footer";
+
 
 export default function App() {
   return (
@@ -12,7 +12,7 @@ export default function App() {
       <IntroSection />
       <AboutMe />
       <MainProjects />
-      <Footer/>
+      <Footer />
     </>
   );
 }
