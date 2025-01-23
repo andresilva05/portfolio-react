bbc7fde695b9307f99fcbdeaa6803659a6b8d4ad
andresilva05 <andresilva9657@gmail.com>
2025-01-22 21:29:06 -0300

    feat: integra imagem de perfil e redes sociais, além de incluir link para download do CV no cabeçalho

--- src/pages/IntroSection/index.jsx
+++ src/pages/IntroSection/index.jsx
@@ -1,7 +1,20 @@
 import { useEffect, useRef, useState } from "react";
-import { ContainerHeader, MyName, StyledNav, Title } from "./styles";
+import {
+  ContainerHeader,
+  MyName,
+  StyledNav,
+  Title,
+  ProfileImage,
+  AboutMeContainer,
+  SocialIcons,
+  GithubLogo,
+  DownloadCv,
+} from "./styles";
 import TypingAnimation from "../../components/TypingContainer";
 import MenuHamburguer from "../../components/MenuHamburguer";
+import AboutMeImg from "../../assets/MeImg.jpg";
+import GithubImg from "../../assets/github-icon.png";
+import { LinkedinLogo, WhatsappLogo, InstagramLogo } from "phosphor-react";
 
 export default function IntroSection() {
   const headingRef = useRef(null); // Referência para o <h1>
@@ -11,20 +24,18 @@ export default function IntroSection() {
     const h1Element = headingRef.current;
 
     if (h1Element) {
-      // Adiciona a classe `visible` após um pequeno delay
+      // Adiciona a classe visible após um pequeno delay
       setTimeout(() => {
         h1Element.classList.add("visible");
-      }, 100); // Ajuste o delay se necessário
+      }, 100);
     }
 
-    // Alterna as cores de "Silva"
     const silvaInterval = setInterval(() => {
       setSilvaColor((prevColor) =>
-        prevColor === "#fa0707" ? "#0e274b" : "#fa0707"
+        prevColor === "#fa0707" ? "#007bff" : "#fa0707"
       );
     }, 2000);
 
-    // Limpa os intervalos ao desmontar o componente
     return () => {
       clearInterval(silvaInterval);
     };
@@ -39,15 +50,54 @@ export default function IntroSection() {
 
         <MenuHamburguer />
       </StyledNav>
-      <MyName>
-        <h1>
-          Olá, sou <br />
-        </h1>
-        <h1 ref={headingRef}>
-          André <span style={{ color: silvaColor }}>Silva</span>
-        </h1>
-        <TypingAnimation />
-      </MyName>
+      <AboutMeContainer>
+        <MyName>
+          <h2>Olá, sou</h2>
+          <h1 ref={headingRef}>
+            {" "}
+            André <span style={{ color: silvaColor }}>Silva</span>
+          </h1>
+          <TypingAnimation />
+          <SocialIcons>
+            <a
+              href="https://www.linkedin.com/in/wilson-andr%C3%A9-de-melo-silva-9b9b39235/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <LinkedinLogo size={32} color="#fa0000" />
+            </a>
+            <a
+              href="https://wa.me/5514998467362"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <WhatsappLogo size={32} color="#fa0000" />
+            </a>
+            <a
+              href="https://github.com/andresilva05"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <GithubLogo src={GithubImg} />
+            </a>
+            <a
+              href="https://www.instagram.com/__andre.silva17/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <InstagramLogo size={32} color="#fa0000" />
+            </a>
+          </SocialIcons>
+
+          <DownloadCv>
+            <a href="../../assets/Curriculo-Front_end.pdf" download="CV-Wilson-André_Desenvolvedor-FrontEnd.pdf">
+              Download CV
+            </a>
+          </DownloadCv>
+        </MyName>
+
+        <ProfileImage src={AboutMeImg} />
+      </AboutMeContainer>
     </ContainerHeader>
   );
 }
