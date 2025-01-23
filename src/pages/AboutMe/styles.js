e93be0e048872055fc3931577adcc356fbbb7274
andresilva05 <andresilva9657@gmail.com>
2025-01-22 21:28:37 -0300

    style: centraliza conteúdo na seção "Sobre mim" e ajusta estilos do texto e título para melhorar legibilidade em dispositivos móveis

--- src/pages/AboutMe/styles.js
+++ src/pages/AboutMe/styles.js
@@ -7,6 +7,7 @@ export const AboutSection = styled.section`
   padding-bottom: 60px;
   padding-top: 70px;
   gap: 60px;
+  text-align: center;
 
   @media (max-width: 768px) {
     flex-direction: column;
@@ -19,29 +20,15 @@ export const AboutSection = styled.section`
   }
 `;
 
-export const ProfileImage = styled.img`
-  width: 400px;
-  height: 380px;
-  border-radius: 100%;
-  border: 2px solid #fa0707;
-  object-fit: fill;
-  margin-left: 10px;
 
-  @media (max-width: 768px) {
-    width: 300px;
-    height: 280px;
-  }
-
-  @media (max-width: 480px) {
-    width: 250px;
-    height: 230px;
-  }
-`;
 
 export const TextSection = styled.div`
   display: flex;
+
   flex-direction: column;
   gap: 20px;
+  justify-content: center;
+  align-items: center;
 
   @media (max-width: 768px) {
     text-align: center;
@@ -52,6 +39,7 @@ export const TextSection = styled.div`
 export const AboutTitle = styled.h2`
   color: white;
   font-size: 75px;
+
 
   @media (max-width: 768px) {
     font-size: 50px;
@@ -66,6 +54,10 @@ export const AboutText = styled.p`
   color: #ffffff;
   font-size: 25px;
   opacity: 0.5;
+  text-align: left;
+  padding: 20px;
+  line-height: 1.2;
+  
 
   @media (max-width: 768px) {
     font-size: 18px;
@@ -73,34 +65,5 @@ export const AboutText = styled.p`
 
   @media (max-width: 480px) {
     font-size: 16px;
-  }
-`;
-
-export const ContactButton = styled.button`
-  color: #ffffff;
-  background: #fa0707;
-  height: 50px;
-  width: 180px;
-  font-size: 18px;
-  border: none;
-  border-radius: 26px;
-  cursor: pointer;
-
-  &:hover {
-    background-color: #000000;
-    border: 3px solid #ee2d2d;
-    transition: 0.5s;
-  }
-
-  @media (max-width: 768px) {
-    width: 150px;
-    height: 45px;
-    font-size: 16px;
-  }
-
-  @media (max-width: 480px) {
-    width: 130px;
-    height: 40px;
-    font-size: 14px;
   }
 `;
