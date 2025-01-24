fb0899cb0262fc96e70cfa1c2eafdb589d6c6801
andresilva05 <andresilva9657@gmail.com>
2025-01-23 21:41:53 -0300

    feat: Refatoração da estilização da seção "Texto" com melhorias no alinhamento

--- src/pages/AboutMe/styles.js
+++ src/pages/AboutMe/styles.js
@@ -20,26 +20,22 @@ export const AboutSection = styled.section`
   }
 `;
 
-
-
 export const TextSection = styled.div`
   display: flex;
-
   flex-direction: column;
   gap: 20px;
   justify-content: center;
   align-items: center;
 
   @media (max-width: 768px) {
-    text-align: center;
-    align-items: center;
+    text-align: center; /* Centraliza o texto da div para telas menores */
+    align-items: center; /* Alinha ao centro os filhos da div */
   }
 `;
 
 export const AboutTitle = styled.h2`
   color: white;
   font-size: 75px;
-
 
   @media (max-width: 768px) {
     font-size: 50px;
@@ -57,7 +53,6 @@ export const AboutText = styled.p`
   text-align: left;
   padding: 20px;
   line-height: 1.2;
-  
 
   @media (max-width: 768px) {
     font-size: 18px;
@@ -65,5 +60,47 @@ export const AboutText = styled.p`
 
   @media (max-width: 480px) {
     font-size: 16px;
+  }
+`;
+
+export const ContainerSkill = styled.div`
+  text-align: center;
+
+  display: flex;
+  flex-direction: column;
+  cursor: pointer;
+
+  h1 {
+    color: #fff;
+  }
+
+  div {
+    display: flex;
+    flex-direction: row;
+    gap: 5px;
+    padding: 10px;
+
+    svg {
+      color: #fa0000;
+      box-shadow: 0 0 10px 4px rgba(250, 7, 7, 0.8);
+      border-radius: 25px;
+      transition: box-shadow 0.3s ease-in-out, transform 0.3s ease-in-out;
+      background-color:rgb(3, 3, 3);
+
+      &:hover {
+        box-shadow: 0 0 15px 6px rgba(250, 7, 7, 1);
+        transform: scale(1.2);
+      }
+    }
+  }
+
+  svg {
+    transition: all 0.3s ease-in-out;
+    width: 32px; /* Tamanho padrão */
+    height: 32px; /* Tamanho padrão */
+
+    &:hover {
+      transform: scale(1.5);
+    }
   }
 `;
