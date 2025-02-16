c2ea36f7428472fea32cc8492e9c616bbe821566
andresilva05 <andresilva9657@gmail.com>
2025-02-16 12:50:23 -0300

    style(footer): ajusta estilos para responsividade e modifica estrutura de layout

--- src/pages/Footer/styles.js
+++ src/pages/Footer/styles.js
@@ -1,13 +1,20 @@
 import styled from "styled-components";
 
 export const InformationFooter = styled.footer`
-  background-color: #000000;
-  padding-top: 100px;
-  padding-bottom: 120px;
+  background: rgb(10, 0, 27);
+  margin-top: -2px;
+  padding: 0 20px 0 20px;
+`;
+
+export const FooterContainer = styled.div`
   display: flex;
-  flex-direction: column;
+  justify-content: space-between;
   align-items: center;
-  justify-content: center;
+  width: 100%;
+  max-width: 1280px;
+  padding: 5rem 1rem;
+  margin: auto;
+  color: rgb(255, 255, 255);
 
   @media (max-width: 768px) {
     padding-top: 80px;
@@ -21,8 +28,8 @@ export const InformationFooter = styled.footer`
 `;
 
 export const Myname = styled.h5`
-  color: #ffffff;
-  font-size: 30px;
+  font-size: 1rem;
+  font-weight: 400;
 
   @media (max-width: 768px) {
     font-size: 26px;
@@ -33,70 +40,14 @@ export const Myname = styled.h5`
   }
 `;
 
-export const InfoText = styled.p`
+export const InfoText = styled.div`
   color: #ffffff;
   font-size: 18px;
-  margin-top: 14px;
-  margin-bottom: 30px;
-
   @media (max-width: 768px) {
     font-size: 16px;
   }
 
   @media (max-width: 480px) {
     font-size: 14px;
-    margin-top: 10px;
-    margin-bottom: 20px;
-  }
-`;
-
-export const ContactLinks = styled.div``;
-
-export const Links = styled.a``;
-
-export const SocialContainer = styled.div`
-  display: flex;
-  gap: 20px;
-
-  img {
-    height: 24px;
-    border-radius: 50%;
-  }
-
-  @media (max-width: 768px) {
-    gap: 15px;
-  }
-
-  @media (max-width: 480px) {
-    gap: 10px;
-  }
-`;
-
-export const SocialLink = styled.a`
-  background-color: #f9004d;
-  padding: 15px;
-  border-radius: 50%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 50px;
-  width: 50px;
-  cursor: pointer;
-
-  &:hover {
-    transform: scale(1.3);
-    transition: transform 0.3s ease;
-  }
-
-  @media (max-width: 768px) {
-    height: 45px;
-    width: 45px;
-    padding: 12px;
-  }
-
-  @media (max-width: 480px) {
-    height: 40px;
-    width: 40px;
-    padding: 10px;
   }
 `;
