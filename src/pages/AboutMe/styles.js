fb37053bbf65c9faff5fd808093e41eccfe10945
andresilva05 <andresilva9657@gmail.com>
2025-02-16 12:44:24 -0300

    git commit -m "style(AboutMe): ajusta padding e text-align, aprimora interatividade dos ícones"

--- src/pages/AboutMe/styles.js
+++ src/pages/AboutMe/styles.js
@@ -50,16 +50,20 @@ export const AboutText = styled.p`
   color: #ffffff;
   font-size: 25px;
   opacity: 0.5;
-  text-align: left;
-  padding: 20px;
+  text-align: justify;
+  padding: 100px; //70px
   line-height: 1.2;
 
   @media (max-width: 768px) {
     font-size: 18px;
+    padding: 50px; //70px
+    text-align: left;
   }
 
   @media (max-width: 480px) {
     font-size: 16px;
+    padding: 30px; //70px
+    text-align: left;
   }
 `;
 
@@ -70,6 +74,7 @@ export const ContainerSkill = styled.div`
   flex-direction: column;
   cursor: pointer;
 
+
   h1 {
     color: #fff;
   }
@@ -77,18 +82,14 @@ export const ContainerSkill = styled.div`
   div {
     display: flex;
     flex-direction: row;
-    gap: 5px;
+    gap: 10px;
     padding: 10px;
 
     svg {
       color: #fa0000;
-      box-shadow: 0 0 10px 4px rgba(250, 7, 7, 0.8);
-      border-radius: 25px;
       transition: box-shadow 0.3s ease-in-out, transform 0.3s ease-in-out;
-      background-color:rgb(3, 3, 3);
 
       &:hover {
-        box-shadow: 0 0 15px 6px rgba(250, 7, 7, 1);
         transform: scale(1.2);
       }
     }
