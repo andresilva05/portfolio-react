bfe73931516fc6aeee21d5451c2533c38cd0d261
andresilva05 <andresilva9657@gmail.com>
2025-02-26 21:49:37 -0300

    .

--- src/pages/IntroSection/index.jsx
+++ src/pages/IntroSection/index.jsx
@@ -89,7 +89,12 @@ export default function IntroSection() {
           </SocialIcons>
 
           <DownloadCv>
-            <a href="/Curriculo-Front_end.pdf">Download CV</a>
+            <a
+              href="/Curriculo-Front_end.pdf"
+              download="CV-Wilson-André_Desenvolvedor-FrontEnd.pdf"
+            >
+              Download CV
+            </a>
           </DownloadCv>
         </MyName>
         <div style={{ width: "min-content" }}>
